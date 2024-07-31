a088961671e58bd4964d277d2f773db6db72365d
Francisco <fhydglan@gmail.com>
2024-07-31 11:46:40 -0300

    created component01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Component01Component} from "./component01/component01.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Component01Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
