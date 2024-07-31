dd67fc3746fcafe9c5f2cdc639a3ef4136826001
Francisco <fhydglan@gmail.com>
2024-07-31 12:00:22 -0300

    Declared two variables and used them to print something on the screen

--- src/app/component01/component01.component.ts
+++ src/app/component01/component01.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-component01',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './component01.component.css'
 })
 export class Component01Component {
+
+  //Variables
+  name: string = "Francisco";
+  age: number = 28;
 
 }
