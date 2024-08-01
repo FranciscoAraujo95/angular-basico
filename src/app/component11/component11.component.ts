b86277d9100a490abf17323d874653e3a269f6ca
Francisco <fhydglan@gmail.com>
2024-07-31 21:41:23 -0300

    Initial validations on the form, required, number of caracteres, min-max, etc

--- src/app/component11/component11.component.ts
+++ src/app/component11/component11.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-component11',
@@ -11,9 +11,9 @@ import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 export class Component11Component {
 
   form = new FormGroup({
-    name  : new FormControl(''),
-    age   : new FormControl(null),
-    city  : new FormControl('')
+    name : new FormControl('', [Validators.required, Validators.minLength(3)]),
+    age  : new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
+    city : new FormControl('', [Validators.required, Validators.minLength(3)])
 
   });
 
